029686661a02f84f2606d027c428b449879fec1e
RajaShanmugavel <raja.karupiah@gmail.com>
2019-04-17 18:47:25 +1000

    misc

--- One/src/main/java/com/abc/raja/EmployeeController.java
+++ One/src/main/java/com/abc/raja/EmployeeController.java
@@ -3,7 +3,15 @@ package com.abc.raja;
 import com.abc.raja.dao.Employee;
 import com.abc.raja.repo.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/employee")
@@ -27,6 +35,16 @@ public class EmployeeController {
     public Employee getEmployee(@PathVariable Integer id) {
         return employeeRepository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
     }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable Integer id) {
+        employeeRepository.deleteById(id);
+    }
+
+    /*@PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable Integer id) {
+
+    }*/
 
 
 }
