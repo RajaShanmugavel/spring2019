07ad079a529f1c70454d3cf805f75a46cf784717
RajaShanmugavel <raja.karupiah@gmail.com>
2019-04-19 21:12:38 +1000

    misc

--- One/src/main/java/com/abc/raja/dao/Employee.java
+++ One/src/main/java/com/abc/raja/dao/Employee.java
@@ -7,7 +7,7 @@ import java.util.Date;
 public class Employee {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name = "name")
